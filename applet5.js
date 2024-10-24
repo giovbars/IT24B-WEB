aa78e13f2b7533ebe855da958323897cf84be615
giovbars <giovannybarsobia04@gmail.com>
2024-10-24 19:46:26 +0800

    Add WeatherService class extending WeatherApp

--- applet5.js
+++ applet5.js
@@ -28,3 +28,8 @@ class WeatherApp{
         this.weatherCard.style.display = 'block';
     }
 }
+class WeatherService extends WeatherApp {
+    constructor() {
+        super();
+        this.apiKey = '';
+    }
