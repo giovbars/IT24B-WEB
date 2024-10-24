e21edeff4f60fb6d530d251921f59cc44d77f3be
giovbars <giovannybarsobia04@gmail.com>
2024-10-25 07:06:12 +0800

    added filterCards

---
+++ index.js
@@ -0,0 +1,13 @@
+class CardSearch {
+    constructor(searchInputId, cardContainerId) {
+        this.searchInput = document.getElementById(searchInputId);
+        this.cardContainer = document.getElementById(cardContainerId);
+        this.cards = this.cardContainer.querySelectorAll('.col-md-4');
+        this.init();
+    }
+    init() {
+        this.searchInput.addEventListener('input', () => {
+            this.filterCards(this.searchInput.value);
+        });
+    }
+    filterCards(query) {
