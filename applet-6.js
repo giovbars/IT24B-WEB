2c223e1048857ca32ccccc3601f3cda688d73f6e
giovbars <giovannybarsobia@gmail.com>
2024-10-27 10:35:57 +0800

    added body

--- applet-6.js
+++ applet-6.js
@@ -38,3 +38,10 @@
       </div>
     </div>
   </nav>
+  <body>
+  
+  <div class="input-group input-group-lg applet-searchbar mb-4">
+      <span class="input-group-text" id="inputGroup-sizing-lg">Search</span>
+      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Type to search...">
+  </div>
+  </nav>
