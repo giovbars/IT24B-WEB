200a2b16dd15c6151d74f8d12d8051e09a9deb57
giovbars <giovannybarsobia@gmail.com>
2024-10-27 10:41:33 +0800

    added TodoList

--- applet-6.js
+++ applet-6.js
@@ -1,60 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Applet 6 - Todo List</title>
-    <style>
-        .applet-container{
-            margin-top: 1rem;
-            margin-left: 2rem;
-        }
-         .completed{
-            text-decoration: line-through;
-            color: gray;
-        }
+class TodoList {
+    constructor() {
+        this.editingIndex = -1;
+        this.addButton = document.getElementById('addButton');
+        this.todoInput = document.getElementById('todoInput');
+        this.todoList = document.getElementById('todoList');
 
-    </style>
-    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.html">Home</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0"></ul>
-        <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Apps
-            </a>
-            <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="applet-1.html">Applet 1 - Basic Events </a></li>
-              <li><a class="dropdown-item" href="applet-2.html">Applet 2 - leaflet Map </a></li>
-              <li><a class="dropdown-item" href="applet-3.html">Applet 3 - Button Logger </a></li>
-  
-            </ul>
-      </div>
-    </div>
-  </nav>
-  <body>
-  
-  <div class="input-group input-group-lg applet-searchbar mb-4">
-      <span class="input-group-text" id="inputGroup-sizing-lg">Search</span>
-      <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg" placeholder="Type to search...">
-  </div>
-  </nav>
-  <div class="container-fluid"  style="max-width: 40%;">
-        <h2 class="text-center">To-Do List</h2>
-        <div class="input-group mb-4">
-            <textarea class="form-control" id="todoInput" placeholder="Add a new task"  rows="3"></textarea>
-            <div class="input-group-append" style="margin-left: .5rem;">
-                <button class="btn btn-primary" id="addButton">Add</button>
-            </div>
-        </div>
-        <ul id="todoList" class="list-group"></ul>
-    </div>
-    <script src="applet-6.js"></script>
-</body>
-</html>
+        this.addButton.addEventListener('click', () => this.addOrUpdateTask());
+        this.todoList.addEventListener('click', (e) => {
+            const action = e.target.classList.contains('removeButton') ? 'remove' : 
+                           e.target.classList.contains('editButton') ? 'edit' : 
+                           e.target.classList.contains('doneButton') ? 'done' : null;
+            if (action) this[action + 'Task'](e);
+        });
+    }
