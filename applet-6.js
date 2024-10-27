73c51cddd4ce19638f7b51d698f3be87aa38808f
giovbars <giovannybarsobia@gmail.com>
2024-10-27 10:32:37 +0800

    added button

--- applet-6.js
+++ applet-6.js
@@ -17,3 +17,9 @@
         }
 
     </style>
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html">Home</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
